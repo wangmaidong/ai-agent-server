946b08f6899fc2ac0ab25ce63ed64515c38a73cc
wanglei <wanglei@unittec.com>
2026-04-22 16:20:59 +0800

    feat: 简历模板功能模块

---
+++ app/model/ResumeTemplateModel.py
@@ -0,0 +1,13 @@
+from sqlmodel import Field
+
+from app.model.BasicModel import BasicModel
+from app.utils.model_utils import FormattedDatetime, FormattedDate, FormattedDecimal
+
+
+class ResumeTemplateModel(BasicModel, table=True):
+  __tablename__ = "llm_resume_template"
+
+  source_code: str | None = Field(default=None, description="简历模板TSX代码")
+  thumb_image: str | None = Field(default=None, description="简历模板缩略图")
+  default_primary: str | None = Field(default=None, description="默认主题色")
+  default_secondary: str | None = Field(default=None, description="默认次级色")
