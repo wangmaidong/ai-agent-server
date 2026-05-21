d5b204e9a5229401b750e24e58140ae07c6c369d
wanglei <2934490605@qq.com>
2026-05-21 21:50:39 +0800

    feat:拦截器注册统一管理

---
+++ app/interceptors/interceptors.py
@@ -0,0 +1,7 @@
+from app.interceptors.add_interceptor_module import add_interceptor_module
+from app.interceptors.add_interceptor_user import add_add_interceptor_user
+
+interceptors = [
+  add_interceptor_module,
+  add_add_interceptor_user,
+]
